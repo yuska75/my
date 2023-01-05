f57c860013ad72a6aa59ceb53c8a2be950be0c57
Yuska <globalfejsn@gmail.com>
2023-01-05 12:30:59 +0300

    add all fonts

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hallo Ihlas');
+function getTimeFromMinutes(minutesTotal) {
+  if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+    return "Ошибка, проверьте данные";
+}
+const hours = Math.floor(minutesTotal / 60);
+const minutes = minutesTotal % 60;
+
+let hoursStr = '';
+
+switch (hours) {
+    case 0: 
+        hoursStr = 'часов';
+        break;
+    case 1:
+        hoursStr = 'час';
+        break;
+    case 2:
+    case 3:
+    case 4:
+        hoursStr = 'часа';
+        break;
+    default:
+        hoursStr = 'часов';
+}
+
+return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+console.log( getTimeFromMinutes(660) );
